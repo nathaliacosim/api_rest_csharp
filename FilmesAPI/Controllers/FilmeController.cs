440f1e1f1219c5a2c316448b11bb901bf2cdc892
nathaliacosim <nathalia.cosim@novaandradina.org>
2023-05-09 17:39:27 -0400

    first code

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,33 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new();
+    private static int id = 0;
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        filme.Id = id++;
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Genero);
+        Console.WriteLine(filme.Duracao);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public Filme? RecuperaFilmePorId(int id)
+    {
+        return filmes.FirstOrDefault(filme => filme.Id == id);
+    }
+} 
